ede5d2121deb8229f848ebb607bdb311fc365d49
NastiaGusev <gusev.nastia@gmail.com>
2025-03-21 18:01:25 +0200

    added homescreen and list for news

--- app/src/main/java/com/loc/newsapp/presentation/common/ArticleCard.kt
+++ app/src/main/java/com/loc/newsapp/presentation/common/ArticleCard.kt
@@ -1,10 +1,113 @@
 package com.loc.newsapp.presentation.common
 
+import android.content.res.Configuration
+import androidx.annotation.ColorRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.loc.newsapp.R
 import com.loc.newsapp.domain.model.Article
+import com.loc.newsapp.domain.model.Source
+import com.loc.newsapp.presentation.Dimens.ArticleCardSize
+import com.loc.newsapp.presentation.Dimens.ExtraSmallPadding
+import com.loc.newsapp.presentation.Dimens.ExtraSmallPadding2
+import com.loc.newsapp.presentation.Dimens.SmallIconSize
+import com.loc.newsapp.ui.theme.NewsAppTheme
 
 @Composable
-fun ArticleCard(modifier: Modifier, article: Article, onClick:() -> Unit) {
+fun ArticleCard(modifier: Modifier, article: Article, onClick: () -> Unit) {
 
+    Row(modifier = modifier.clickable { onClick() }) {
+        AsyncImage(
+            modifier = Modifier
+                .size(ArticleCardSize)
+                .clip(MaterialTheme.shapes.medium),
+            model = ImageRequest.Builder(LocalContext.current).data(article.urlToImage).build(),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .padding(horizontal = ExtraSmallPadding)
+                .height(ArticleCardSize)
+        ) {
+            Text(
+                text = article.title,
+                style = MaterialTheme.typography.bodyMedium,
+                color = colorResource(id = R.color.text_title), maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = article.source.name,
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Bold),
+                    color = colorResource(id = R.color.text_title)
+                )
+
+                Spacer(modifier = Modifier.width(ExtraSmallPadding2))
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_time),
+                    contentDescription = null,
+                    modifier = Modifier.size(SmallIconSize),
+                    tint = colorResource(R.color.body)
+                )
+                Spacer(modifier = Modifier.width(ExtraSmallPadding2))
+                Text(
+                    text = article.publishedAt,
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Bold),
+                    color = colorResource(id = R.color.text_title)
+                )
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun ArticleCardPreview() {
+    NewsAppTheme {
+        ArticleCard(
+            article = Article(
+                author = "Matthew Gault",
+                content = "Can a President make money out of thin air? On paper, yes.\r\nDonald Trump and Elon Musk have been talking a lot about Fort Knox lately, the place where America keeps its official gold reserves. Both h… [+3792 chars]",
+                description = "More than a stunt, the Fort Knox visit might be a chance for the President to change the price of gold and dump the price hike into cryptocurrency.",
+                publishedAt = "2025-02-27T19:05:24Z",
+                source = Source(id = "null", name = "Gizmodo.com"),
+                title = "Musk and Trump’s Fort Knox Trip Is About Bitcoin",
+                url = "https://gizmodo.com/musk-and-trumps-fort-knox-trip-is-about-bitcoin-2000569420",
+                urlToImage = "https://gizmodo.com/app/uploads/2024/10/sec-bitcoin-hack-arrest.jpg"
+            ),
+            modifier = Modifier
+        ) {
+
+        }
+    }
 }
--- app/src/main/java/com/loc/newsapp/presentation/nvgraph/NavGraph.kt
+++ app/src/main/java/com/loc/newsapp/presentation/nvgraph/NavGraph.kt
@@ -1,12 +1,14 @@
 package com.loc.newsapp.presentation.nvgraph
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.loc.newsapp.presentation.home.HomeScreen
+import com.loc.newsapp.presentation.home.HomeViewModel
 import com.loc.newsapp.presentation.onBoarding.OnBoardingScreen
 import com.loc.newsapp.presentation.onBoarding.OnBoardingViewModel
 
@@ -27,9 +29,12 @@ fun NavGraph(startDestination: String) {
         }
 
         navigation(route = Route.NewsNavigation.route,
-            startDestination = Route.NewsNavigatorScreen.route) {
+            startDestination = Route.NewsNavigatorScreen.route
+        ) {
             composable(route = Route.NewsNavigatorScreen.route) {
-                Text(text = "News Navigator Screen")
+                val viewModel: HomeViewModel = hiltViewModel()
+                val articles = viewModel.news.collectAsLazyPagingItems()
+                HomeScreen(articles = articles, navigate = {})
             }
         }
     }
